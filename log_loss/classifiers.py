77f5af8457616c0e212e04ccf175a18f71d9077c
Alex Ayoub <43243047+aa14k@users.noreply.github.com>
2024-01-21 13:22:23 -0700

    Add files via upload

---
+++ log_loss/classifiers.py
@@ -0,0 +1,112 @@
+import numpy as np
+import scipy as sc
+from scipy.optimize import least_squares
+from tqdm.notebook import tqdm
+from sklearn.linear_model import LogisticRegression
+
+
+class Sigmoid_Regression(object):
+    def __init__(self, features, obs, d, theta_star, lr_tol = pow(10,-8)):
+        self.features = features
+        self.obs = obs
+        self.n = len(obs)
+        self.d = d
+        self.theta_star = theta_star
+        self.logit_regression_tol = lr_tol
+        theta_init = np.random.normal(size=self.d)
+        
+        self.theta_ls = theta_init
+        self.theta = theta_init
+        
+ 
+    def solve_LS(self):
+        #Solve nonlinear least squares using scipy black-box (since sq loss is non-convex wrt to theta)
+        #A = np.zeros((self.d,self.d))
+        #b = np.zeros(self.d)
+        #for i in range(self.n):
+        #    x, y = self.features[i], self.obs[i]
+        #    A = A + np.outer(x, x)
+        #    b = b + y * x
+        #self.theta_ols = np.linalg.solve(A,b)
+        sol = least_squares(self.func, self.theta_ls, args=(self.features,self.obs))
+        self.theta_ls = sol.x
+    
+    def solve_Logit_Regression(self):
+        self.clf = LogisticRegression(tol = self.logit_regression_tol, random_state = 0).fit(self.features, self.obs)
+        return np.array(self.clf.coef_)
+
+
+
+    def sigmoid(self, x):
+        x[x < -36] = -36
+        x[x > 36] = 36
+        return 1 / (1 + np.exp(-x))
+        
+    def objective_function(self, theta, X, Y):
+        p = self.sigmoid(X @ theta)
+        return -1.0 * np.sum(Y * np.log(p) + (1 - Y) * np.log(1 - p))
+    
+    
+    def logistic_regression(self):
+        self.res = sc.optimize.minimize(
+            self.objective_function,
+            x0 = np.zeros(self.d),
+            args=(self.features,self.obs),
+            tol = pow(10,-8)
+        )
+        return self.res.x
+        
+    
+    def get_grad_hessian_log(self,theta):
+        
+        inner = np.dot(self.features, self.theta)
+        grad = np.zeros(self.d)
+        X = self.features.copy()
+        
+        mu = np.zeros((self.n,self.d))
+        mu[:,0] = -1.0 * (2 * self.obs - np.tanh(inner) - 1)
+        dot_mu_root = np.sqrt(1 / np.cosh(inner))
+        
+        
+        for i in range(self.d):
+            if i != 0:
+                mu[:,i] = mu[:,0]
+            X[:,i] = np.multiply(X[:,i], dot_mu_root)
+            
+        mult = np.multiply(mu,self.features)
+        grad = np.sum(mult, axis = 0)
+        
+        hess = np.einsum('ij,ik->jk', X, X)
+        
+        return hess, grad
+    
+   
+    def p(self,x):
+        x = np.where(x >= 500, 500, x)
+        x = np.where(x <= -500, -500, x)
+        return 1 / (1 + np.exp(-x))
+    
+    def func(self, theta, x, y):
+        # Return residual = fit-observed
+        return self.p(np.inner(x,theta)) - y
+        
+
+    
+    def Newton_Method(self):
+        
+        print(np.linalg.norm(self.theta_ls - self.theta_star))
+        
+        #Solve logisitic regression using newton's method (since log loss is convex wrt to theta)
+        for i in tqdm(range(self.newton_iters)):
+            
+            hess, grad = self.get_grad_hessian_log(self.theta)
+            #hess_ls, grad_ls = self.get_grad_hessian_sq(self.theta_ls)
+            
+            newton_step = np.linalg.solve(hess, grad)
+            #newton_step_ls = np.linalg.solve(hess_ls, grad_ls)
+            
+            self.theta = self.theta - newton_step
+            #self.theta_ls = self.theta_ls - newton_step_ls
+            
+            if i % 1000 == 999:
+                print(np.linalg.norm(newton_step))
---
+++ log_loss/utils.py
@@ -0,0 +1,223 @@
+from environments import MountainCar
+from features import LinearFeatureMap
+import numpy as np
+from tqdm.notebook import tqdm
+import itertools as iters
+from joblib import Parallel, delayed
+from scipy.optimize import least_squares
+import scipy as sc
+import timeit
+import copy
+import matplotlib.pyplot as plt
+from fitted_q import FittedQIteration
+
+def move_successful_trajectories(tuples, H):
+    x = np.where(tuples[-1][2] == 0)
+    if len(x) == 1:
+        idx = x[0][0]
+        for h in range(H):
+            s,a,c,s_ = tuples[h][0][idx], tuples[h][1][idx], tuples[h][2][idx], tuples[h][3][idx]
+            s1,a1,c1,s_1 = tuples[h][0][0], tuples[h][1][0], tuples[h][2][0], tuples[h][3][0]
+            tuples[h][0][0], tuples[h][1][0], tuples[h][2][0], tuples[h][3][0] = s,a,c,s_
+            tuples[h][0][idx], tuples[h][1][idx], tuples[h][2][idx], tuples[h][3][idx] = s1,a1,c1,s_1 
+        
+    else:
+        idx = x[0]
+        v = range(len(idx))
+        for h in range(H):
+            s,a,c,s_ = tuples[h][0][idx], tuples[h][1][idx], tuples[h][2][idx], tuples[h][3][idx]
+            s1,a1,c1,s_1 = tuples[h][0][v], tuples[h][1][v], tuples[h][2][v], tuples[h][3][v]
+            tuples[h][0][v], tuples[h][1][v], tuples[h][2][v], tuples[h][3][v] = s,a,c,s_
+            tuples[h][0][idx], tuples[h][1][idx], tuples[h][2][idx], tuples[h][3][idx] = s1,a1,c1,s_1 
+            
+    return tuples
+
+
+def truncate_data(tuples, H, num_trials):
+    tuples_new = []
+    for h in range(H):
+        tuples_new.append([tuples[h][0][:num_trials],tuples[h][1][:num_trials],tuples[h][2][:num_trials],tuples[h][3][:num_trials],tuples[h][4]])
+    return tuples_new
+
+
+def get_data(H, num_trials, num_success=None):
+    for x in range(5000):
+        
+        if num_success == None:
+            num_success = 1
+            
+        env = MountainCar(H)
+        var = 0.0
+        s = np.zeros((2,num_trials))
+        s[0,:] = np.ones(num_trials) * - 0.5
+        #s[0,:] = np.random.uniform(low = -1.2, high = 0.6, size = num_trials)
+        env.reset()
+        tuples = []
+        
+        for h in (range(H)):
+            a = np.random.choice([-1,0,1],size=num_trials)
+            cost, s_ = env.step_broadcast(s, a, num_trials, var)
+            tuples.append([s.T,a+1,cost,np.array(s_).T,h])
+            s = s_
+        x = np.where(tuples[H-1][2]==0)
+        if x[0].shape[0] >= num_success:
+            return tuples
+            break
+
+def evaluate_policy(policy,H, var, theta1, theta2, phi): 
+    
+    if policy == 'log':
+        num_trials = 1
+        env = MountainCar(H)
+        
+        s = np.zeros((2,num_trials))
+        s[0,:] = np.ones(num_trials) * - 0.5
+        #s[0,:] = np.random.uniform(low = -1.2, high = 0.6, size = num_trials)
+        env.reset()
+        costs = []
+        s_vec = []
+        for h in (range(H)):
+            X = phi.fourier_basis(s.T)
+            q = np.zeros((num_trials,3))
+            for a in range(3):
+                q[:,a] = X @ theta1[h,a]
+            a = np.argmin(q, axis=1)
+            cost, s_ = env.step_broadcast(s, a, num_trials, var)
+            #tuples.append([s.T,a+1,cost,np.array(s_).T,h])
+            costs.append(cost)
+            s = s_
+            s_vec.append(s)
+    else:
+        
+        num_trials = 1
+        env = MountainCar(H)
+        
+        s = np.zeros((2,num_trials))
+        s[0,:] = np.ones(num_trials) * - 0.5
+        env.reset()
+        costs = []
+        s_vec = []
+        for h in (range(H)):
+            X = phi.fourier_basis(s.T)
+            q = np.zeros((num_trials,3))
+            for a in range(3):
+                q[:,a] = X @ theta2[h,a]
+            a = np.argmin(q, axis=1)
+            cost, s_ = env.step_broadcast(s, a, num_trials, var)
+            costs.append(cost)
+            s = s_
+            s_vec.append(s)
+        
+    return costs,s_vec
+
+
+
+def evaluate_steps(policy, H, var, theta1, theta2, phi): 
+    
+    if policy == 'log':
+        num_trials = 1
+        env = MountainCar(H)
+        
+        s = np.zeros((2,num_trials))
+        s[0,:] = np.ones(num_trials) * - 0.5
+        #s[0,:] = np.random.uniform(low = -1.2, high = 0.6, size = num_trials)
+        env.reset()
+        costs = []
+        s_vec = []
+        step = H + 1
+        for h in (range(H)):
+            X = phi.fourier_basis(s.T)
+            q = np.zeros((num_trials,3))
+            for a in range(3):
+                q[:,a] = X @ theta1[h,a]
+            a = np.argmin(q, axis=1)
+            cost, s_ = env.step_broadcast(s, a, num_trials, var)
+            
+            if s[0] >= 0.6:
+                step = min(step,h)
+                
+            costs.append(cost)
+            s = s_
+            s_vec.append(s)
+    else:
+        
+        num_trials = 1
+        env = MountainCar(H)
+        step = H + 1
+        s = np.zeros((2,num_trials))
+        s[0,:] = np.ones(num_trials) * - 0.5
+        env.reset()
+        costs = []
+        s_vec = []
+        for h in (range(H)):
+            X = phi.fourier_basis(s.T)
+            q = np.zeros((num_trials,3))
+            for a in range(3):
+                q[:,a] = X @ theta2[h,a]
+            a = np.argmin(q, axis=1)
+            cost, s_ = env.step_broadcast(s, a, num_trials, var)
+            
+            if s[0] >= 0.6:
+                step = min(step,h)
+                
+            costs.append(cost)
+            s = s_
+            s_vec.append(s)
+        
+    return costs, s_vec, step
+
+def run_experiment(H, num_trials, phi, num_success, gamma = 1.0):
+    tuples = get_data(H, num_trials, num_success)
+    features = 'fourier'
+    agent = FittedQIteration(phi, features, tuples, H, num_trials, gamma)
+    theta1 = agent.update_Q_log()
+    theta2 = agent.update_Q_sq()
+    var = 0.0
+    cost_log,s_vec_log = evaluate_policy('log', H, var, theta1, theta2, phi)
+    cost_sq,s_vec_sq = evaluate_policy('sq', H, var, theta1, theta2, phi)
+    
+    return [step_log,step_sq]
+
+
+def run_experiment_horizon(H, num_trials, phi, num_success, gamma = 1.0):
+    tuples = get_data(H, num_trials, num_success)
+    features = 'fourier'
+    agent = FittedQIteration(phi,features,tuples,H,num_trials, gamma)
+    theta1 = agent.update_Q_log()
+    #theta2 = agent.update_Q_sq()
+    theta2 = theta1
+    var = 0.0
+    cost_log,s_vec_log,step_log = evaluate_steps('log', H, var, theta1, theta2, phi)
+    cost_sq,s_vec_sq,step_sq = evaluate_steps('sq', H, var, theta1, theta2, phi)
+    
+    return [step_log, step_sq]
+
+
+def run_experiment2(tup, H, num_trials, phi):
+    features = 'fourier'
+    agent = FittedQIteration(phi,features,tup,H,num_trials)
+    theta1 = agent.update_Q_log()
+    theta2 = agent.update_Q_sq()
+    var = 0.0
+    cost_log,s_vec_log = evaluate_policy('log', H, var, theta1, theta2, phi)
+    cost_sq,s_vec_sq = evaluate_policy('sq', H, var, theta1, theta2, phi)
+    
+    return [(sum(cost_log)), sum(sum(cost_sq))]
+
+
+def fixed_trajectory_loop(data, tuples, phi, runs, H, num_trials, njob):
+    c = []
+    for i in tqdm(range(len(data))):
+        tic = timeit.default_timer()
+        if data[i] != num_trials:
+            tups = []
+            for k in (range(runs)):
+                tups.append(truncate_data(tuples[k], H, data[i]))
+            x = Parallel(n_jobs= njob)(delayed(run_experiment2)(tups[j], H, data[i],phi) for j in range(runs))
+        else:
+            x = Parallel(n_jobs = njob)(delayed(run_experiment2)(tuples[j], H, data[i],phi) for j in tqdm(range(runs)))
+        toc = timeit.default_timer()
+        print('Time: %ss' %(toc-tic))
+        c.append(x)
+    return c
+        
