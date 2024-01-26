0d4dcb74aee6c3828cdde2e877c65a767b8de630
aa14k <aayoub@ualberta.ca>
2024-01-26 09:23:01 -0700

    added bernoullis

--- log_loss/environments.py
+++ log_loss/environments.py
@@ -110,7 +110,7 @@ class MountainCar(object):
         vel = np.where(vel >= 0.07, 0.07, vel)
         
         #vel_top_idx = np.where(vel >= 0.07)
-        #vel[vel_bottom_idx] = -0.07
+        #vel[vel_bottom_idx] = -0.07  
         #vel[vel_top_idx] = 0.07
         
         cost = np.zeros(n)
